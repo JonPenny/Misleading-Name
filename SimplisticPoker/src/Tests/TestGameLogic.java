15a4bff7f79e3dae4774d30fbae628855ffb52b9
jonpenny <jon_penny@hotmail.com>
2015-09-24 17:44:45 -0400

    Added test framework and classes

---
+++ SimplisticPoker/src/Tests/TestGameLogic.java
@@ -0,0 +1,50 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import PokerExceptions.*;
+import junit.framework.Assert;
+
+public class TestGameLogic {
+	@Test
+	public void testDuplicates(){
+		//Make sure that the same card doesn't appear twice
+		
+	}
+	
+	@Test
+	public void testNumberPlayers(){
+		//Test that only 2,3,4 player can play
+		
+	}
+	
+	@Test
+	public void testCardsPerPlayer(){
+		//Make sure that each player submits 5 cards
+		
+	}
+	
+	@Test
+	public void testHandRanks(){
+		//Ensure that cards are ranked properly
+	
+	}
+	
+	@Test
+	public void testDuplicateHands(){
+		//Make sure that hands are ranked equal
+		
+	}
+
+	@Test
+	public void testPlayerIds(){
+		//Ensure that that ids given are used
+
+	}
+
+	@Test
+	public void testOrderEntry(){
+		//make sure that subissions are id then cards
+	}
+	
+}
