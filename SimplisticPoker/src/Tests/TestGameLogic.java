08e8920d7bf1fa72d9ec89ef78618302a8b9e75c
jonpenny <jon_penny@hotmail.com>
2015-09-24 19:33:22 -0400

    Update Test to be more comprehensive

--- SimplisticPoker/src/Tests/TestGameLogic.java
+++ SimplisticPoker/src/Tests/TestGameLogic.java
@@ -8,62 +8,58 @@ import junit.framework.Assert;
 
 public class TestGameLogic {
 	PokerGame game;
-	game = new PokerGame();
-	
+
+	@SuppressWarnings("deprecation")
 	@Test
-	public void testDuplicates(){
-		//Make sure that the same card doesn't appear twice
+	public void testDuplicates() {
+		// Make sure that the same card doesn't appear twice
+		
+		game = new PokerGame();
+		try {
+			game.setNumPlayers(2);
+			game.addHand(1, "AceSpades", "AceSpades", "AceSpades", "AceSpades", "AceSpades");
+			game.addHand(2, "AceSpades", "AceSpades", "AceSpades", "AceSpades", "AceSpades");			
+			Assert.fail();
+		} catch (DuplicateCardException e) {
+			
+		}
 		game.reset();
-		game.setPlayers(2);
-		game.addHand(1,"AceSpades","AceSpades","AceSpades","AceSpades","AceSpades");
-		game.addHand(2,"AceSpades","AceSpades","AceSpades","AceSpades","AceSpades");
-		String result = game.getResults();
-		
-		Assert.assertTrue(result.equals("Invalid Hands - Duplicat Cards"));
-		
-		game.reset();
-		game.setPlayers(2);
-		game.addHand(1,"AceSpades","AceSpades","AceSpades","AceSpades","AceSpades");
-		game.addHand(2,"AceSpades","AceSpades","AceSpades","AceSpades","AceSpades");
-		String result = game.getResults();
-		
-		Assert.assertTrue(result.equals("Invalid Hands - Duplicat Cards"));
+		try {
+			game.setNumPlayers(2);
+			game.addHand(1, "AceSpades", "AceHearts", "AceClubs", "AceDiamonds", "TwoSpades");
+			game.addHand(2, "ThreeSpades", "FourHearts", "AceClubs", "FiveDiamonds", "SixSpades");
+		} catch (DuplicateCardException e) {
+			Assert.fail();
+		}
+
 	}
-	
+
 	/**
-	@Test
-	public void testNumberPlayers(){
-		//Test that only 2,3,4 player can play
-		
-	}
-	
-	@Test
-	public void testCardsPerPlayer(){
-		//Make sure that each player submits 5 cards
-		
-	}
-	
-	@Test
-	public void testHandRanks(){
-		//Ensure that cards are ranked properly
-	
-	}
-	
-	@Test
-	public void testDuplicateHands(){
-		//Make sure that hands are ranked equal
-		
-	}
-
-	@Test
-	public void testPlayerIds(){
-		//Ensure that that ids given are used
-
-	}
-
-	@Test
-	public void testOrderEntry(){
-		//make sure that subissions are id then cards
-	}
-	**/
+	 * @Test public void testNumberPlayers(){ //Test that only 2,3,4 player can
+	 *       play
+	 * 
+	 *       }
+	 * 
+	 * @Test public void testCardsPerPlayer(){ //Make sure that each player
+	 *       submits 5 cards
+	 * 
+	 *       }
+	 * 
+	 * @Test public void testHandRanks(){ //Ensure that cards are ranked
+	 *       properly
+	 * 
+	 *       }
+	 * 
+	 * @Test public void testDuplicateHands(){ //Make sure that hands are ranked
+	 *       equal
+	 * 
+	 *       }
+	 * 
+	 * @Test public void testPlayerIds(){ //Ensure that that ids given are used
+	 * 
+	 *       }
+	 * 
+	 * @Test public void testOrderEntry(){ //make sure that subissions are id
+	 *       then cards }
+	 **/
 }
