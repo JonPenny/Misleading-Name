9ddf8a5efe77d026964c34d9cefd29cf48558f4f
jonpenny <jon_penny@hotmail.com>
2015-09-27 19:30:35 -0400

    Added functionality to calculate score for high card

--- SimplisticPoker/src/PokerGame/Hand.java
+++ SimplisticPoker/src/PokerGame/Hand.java
@@ -1,58 +1,107 @@
 package PokerGame;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import PokerExceptions.DuplicateCardException;
 import PokerExceptions.UnrecognizedCardException;
 
-public class Hand {
-	private static String[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack",
-			"queen", "king", "ace" };
+public class Hand implements Comparable {
+	private static String[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+			"jack", "queen", "king", "ace" };
 	private static String[] suits = { "hearts", "diamonds", "clubs", "spades" };
 
 	private static Set<String> allCards = new HashSet<String>();
+	private int value = 0;
 
 	public static void resetCards() {
 		allCards = new HashSet<String>();
 	}
 
+	private int player;
 	Set<String> cards;
 
-	public static void checkValidity(Set<String> cards) throws DuplicateCardException, UnrecognizedCardException{
+	public static void checkValidity(Set<String> cards) throws DuplicateCardException, UnrecognizedCardException {
 		// see if hand has any duplicates
-				if (cards.size() != 5) {
-					throw new PokerExceptions.DuplicateCardException();
-				}
-				// check if there are duplicates among hands
-				if (Collections.disjoint(allCards, cards)) {
-					allCards.addAll(cards);
-				} else {
-
-					throw new PokerExceptions.DuplicateCardException();
-				}
-				// check that he inputed cards are valid
-				for (String card : cards) {
-					boolean exists = false;
-					for(String suit: suits){
-						for(String num: numbers){
-							if((num+suit).equals(card.toLowerCase())){
-								exists=true;
-								break;
-							}
-						}
-						if(exists){
-							break;
-						}
-					}
-					if(exists==false){
-						throw new PokerExceptions.UnrecognizedCardException();
+		if (cards.size() != 5) {
+			throw new PokerExceptions.DuplicateCardException();
+		}
+		// check if there are duplicates among hands
+		if (Collections.disjoint(allCards, cards)) {
+			allCards.addAll(cards);
+		} else {
+			throw new PokerExceptions.DuplicateCardException();
+		}
+		// check that he inputed cards are valid
+		for (String card : cards) {
+			boolean exists = false;
+			for (String suit : suits) {
+				for (String num : numbers) {
+					if ((num + suit).equals(card.toLowerCase())) {
+						exists = true;
+						break;
 					}
 				}
+				if (exists) {
+					break;
+				}
+			}
+			if (exists == false) {
+				throw new PokerExceptions.UnrecognizedCardException();
+			}
+		}
 
 	}
-	public Hand(String carda, String cardb, String cardc, String cardd, String carde) throws DuplicateCardException, UnrecognizedCardException {
+
+	private int calculateValue() {
+		String[] hand = cards.toArray(new String[cards.size()]);
+		// check for Royal Flush (straight ten to ace, same suit) 23
+
+		// check for straight flush (straight, same suit) 22
+
+		// check for flush 19
+
+		// check for straight 18
+
+		// check for four of a kind (hiest single card) 21
+		// check for full house(3 of a kind + 2 of a kind) 20
+		// check for three of a kind 17
+		// check for Two pair 16
+		// check for one pair 15
+
+		// check for high card 0-14 0;
+		{
+			int high = 0;
+			for (int i = 0; i < numbers.length; i++) {
+				for (String cc : hand) {
+
+					if (cc.toLowerCase().contains(numbers[numbers.length - i - 1])) {
+						if (numbers.length - i > high) {
+							System.out.println(cc + (numbers[numbers.length - i - 1]));
+							high = numbers.length - i;
+						}
+					}
+				}
+			}
+			return high;
+
+		}
+	}
+
+	public int getValue() {
+		if (value == 0) {
+			value = calculateValue();
+		}
+		return value;
+	}
+
+	public Hand(int player, String carda, String cardb, String cardc, String cardd, String carde)
+			throws DuplicateCardException, UnrecognizedCardException {
+		this.player = player;
 		cards = new HashSet<String>();
 		cards.add(carda);
 		cards.add(cardb);
@@ -60,6 +109,38 @@ public class Hand {
 		cards.add(cardd);
 		cards.add(carde);
 		checkValidity(cards);
-		
+
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Hand) {
+			if (getValue() > ((Hand) o).getValue()) {
+				return -1;
+			}
+			if (getValue() == ((Hand) o).getValue()) {
+				return 0;
+			}
+			if (getValue() < ((Hand) o).getValue()) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+	public String toString() {
+		String string = "Player " + player + " Score:" + getValue() + " Hand ";
+		for (String i : cards) {
+			string += i + " ";
+		}
+		return string;
 	}
 }
--- SimplisticPoker/src/PokerGame/PokerGame.java
+++ SimplisticPoker/src/PokerGame/PokerGame.java
@@ -1,20 +1,25 @@
 package PokerGame;
 
+import java.util.Arrays;
+
 import PokerExceptions.DuplicateCardException;
 import PokerExceptions.PlayerNumberException;
 import PokerExceptions.UnrecognizedCardException;
 
 public class PokerGame {
 	Hand[] hands;
-
-	public void reset() {
+	public PokerGame(){
+		reset();
+	}
+	
+	private void reset() {
 		hands = null;
 		Hand.resetCards();
 	}
 
 	public void addHand(int playerId, String carda, String cardb, String cardc, String cardd, String carde)
 			throws DuplicateCardException, UnrecognizedCardException {
-		hands[playerId - 1] = new Hand(carda, cardb, cardc, cardd, carde);
+		hands[playerId - 1] = new Hand(playerId ,carda, cardb, cardc, cardd, carde);
 	}
 
 	public void setNumPlayers(int numPlayers) throws PlayerNumberException {
@@ -25,12 +30,14 @@ public class PokerGame {
 		hands = new Hand[numPlayers];
 	}
 
-	public void getResults() throws PlayerNumberException {
+	public Hand[] getResults() throws PlayerNumberException {
 		for(int i=0; i<hands.length; i++){
 			if(hands[i]==null){
 				reset();
 				throw new PokerExceptions.PlayerNumberException();
 			}
 		}
+		Arrays.sort(hands);
+		return hands;
 	}
 }
